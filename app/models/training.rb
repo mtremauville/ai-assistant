1bbc0be89f7a0b22044ea17805a735efd8cbcabb
Perrine Villers <26814178+Floopidou@users.noreply.github.com>
2026-03-03 15:19:52 +0100

    Model corrected and seed added

--- app/models/training.rb
+++ app/models/training.rb
@@ -2,7 +2,7 @@ class Training < ApplicationRecord
   belongs_to :user
   belongs_to :chat
 
-  validates :duration, :type, :intensity, :team_size, presence: true
+  validates :duration, :training_type, :intensity, :team_size, presence: true
   validates :intensity, numericality: { in: (0..10) }
   validates :feedback_rating, numericality: { in: (0..10) }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,9 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
+  resources :chats, only: :show do
+    resources :messages, only: :create
+  end
 
+  resources :trainings, only: [:index, :show]
 end
