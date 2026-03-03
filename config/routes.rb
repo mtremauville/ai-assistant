8d6fc3972671044e1842efca7badbf7e0e22ad23
Habib Kettani <habibkettani03@gmail.com>
2026-03-03 19:04:28 +0100

    Insertion de la navbar pour se deplacer, depuis la biblio de trainings

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   authenticated :user do
-    root to: "pages#landing", as: :authenticated_root
+    root to: "trainings#index", as: :authenticated_root
   end
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
