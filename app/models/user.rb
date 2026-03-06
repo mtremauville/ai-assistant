8403fc23118be3ad4aeb0a1a505e33784ad80ae4
Perrine Villers <26814178+Floopidou@users.noreply.github.com>
2026-03-06 12:36:51 +0100

    fixed login direction

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :trainings, dependent: :destroy
   has_many :chats, dependent: :destroy
 
-  validates :email, :password, :padel_level, :age, presence: true
+  validates :email, :padel_level, :age, presence: true
   validates :hand_position, inclusion: { in: ["Left", "Right", "Both"] }
   validates :gender, inclusion: { in: ["male", "female", "other"] }, allow_nil: true
   validates :padel_level, numericality: { less_than_or_equal_to: 7.0 }
